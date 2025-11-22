8d9df1ff45ecccb55bee0932b6b86c4a8deb9894
Vineetnaik1611 <vnn28@bath.ac.uk>
2025-11-22 19:49:29 +0000

    second commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT='-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
+    return requirements
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Vineet',
+    author_email='vnn28@bath.ac.uk',
+    packages=find_packages(where='src'),  # Look for packages in 'src' directory
+    package_dir={'': 'src'},              # Tell setuptools that packages are under 'src'
+    install_requires=get_requirements('requirements.txt')
+)
